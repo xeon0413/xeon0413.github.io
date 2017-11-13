aa863a14e6b5d2be9ddc2f4daa08d2a8eff092d1
xeon0413 <32428638+xeon0413@users.noreply.github.com>
2017-11-13 17:23:11 +0900

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -28,9 +28,9 @@ window.onload = function () {
   for (var i = 0; i < sendBtns.length; i++) {
         sendBtns[i].addEventListener('click', function(e) {
 
-          //  var address = document.getElementById('receive_address').value;
+           var address = document.getElementById('receive_address').value;
     
-          var address= 0x1359821c5a98e7d5aab0fa3819a47b74e0411b2e 
+          //var address= 0x1359821c5a98e7d5aab0fa3819a47b74e0411b2e 
           if(address.search(/0x[a-zA-Z0-9]{40}/) != -1) {
         
               var contractAddress = '0xb20083039a3b7b76c0dc3884c6e5f41c3784671d';
